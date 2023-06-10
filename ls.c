faeb55fa42f85d1fb934bc2eb8caa2e0293a38a7
2020864003 <128778304+2020864003@users.noreply.github.com>
2023-06-10 20:02:16 +0900

    ls.c

---
+++ ls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ret = system("ls -l");
+
+    printf("ret : %d \n", ret);
+
+    return 0;
+}
+~                                                                               
+~         
